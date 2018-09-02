4542ed0f64aa3e159c78f66f51d5f9631c931720
Byeongguk Gong <byeonggukgong@gmail.com>
2018-09-02 22:26:07 +0900

    Add task model in tasks app

--- donebox/tasks/models.py
+++ donebox/tasks/models.py
@@ -1,3 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from django.utils import timezone
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=140)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
