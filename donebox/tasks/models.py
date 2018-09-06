fff850c113ab8a328d5d7482a6594b35a9723a3b
Byeongguk Gong <byeonggukgong@gmail.com>
2018-09-06 18:31:37 +0900

    Alter field created_at in task model

--- donebox/tasks/models.py
+++ donebox/tasks/models.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
-from django.utils import timezone
 
 
 class Task(models.Model):
     title = models.CharField(max_length=140)
     is_completed = models.BooleanField(default=False)
-    created_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
